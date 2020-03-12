2ae4ce60a0ad80d38894abd3bd5357386b3b5726
Bailey McKelway <bjlmckelway@gmail.com>
2020-03-12 11:13:42 -0400

    START

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+app.get("/", (req, res) => {
+    console.log("Hello World")
+  })
+
+
+
+const PORT = 1337;
+
+app.listen(PORT, () => {
+console.log(`App listening in port ${PORT}`);
+});
