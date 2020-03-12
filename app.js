2fba15d3a807729f7308556b35434a940306cbb2
Bailey McKelway <bjlmckelway@gmail.com>
2020-03-12 16:17:15 -0400

    DATABASE CONNECTED

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const client = require("./db");
+const { db } = require('./models');
 const main = require('./views/main')
 
 const app = express();
@@ -9,6 +9,15 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: false}))
+
+
+
+
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
 
 app.get("/", (req, res) => {
@@ -19,6 +28,19 @@ app.get("/", (req, res) => {
 
 const PORT = 3000;
 
+
+//const models = require('./models')
+
+
+// const init = async () => {
+//     await models.db.sync({force: true})
+//     app.listen(PORT, () => {
+//         console.log(`Server listening in port ${PORT}`);
+//     })
+// }
+
+// init();
+
 app.listen(PORT, () => {
-console.log(`App listening in port ${PORT}`);
-});
+    console.log(`App listening in port ${PORT}`);
+})
